5127dbaf5f8bcc18b68a2a9f2ee49013684cdeae
muhammad ameen <muhammadameenh1@gmail.com>
2021-05-30 01:01:04 +0500

    Lab Practice

---
+++ 6 to 9 javascript/app.js
@@ -0,0 +1,43 @@
+// //! Chapter 6 to 9
+
+// //*  Question No 3
+
+// var take = prompt("Enter Your Name");
+
+// var result = ( "hello " + take);
+// document.write(result)
+
+// //*  Question No 5
+
+// var take = +prompt("Enter a Number");
+
+// var i = 1;
+// for (i; i<=10; i++){
+//     document.write(take + " * " + i + "=" + take*i + "<br>")
+// }
+
+// //*  Question No 6
+
+// var first_subject = prompt("Enter Your First Subject");
+// var second_subject = prompt("Enter Your second Subject");
+
+// var totalMark = 100;
+
+// var first_obtain = +prompt("Enter Your First Subject obtained Mark");
+// var second_obtain = +prompt("Enter Your second Subject Obtained Mark");
+
+
+// var result = (first_subject + " " + totalMark +  " " + first_obtain + " " + (first_obtain/totalMark) * 100 + "%");
+// var result2 = (second_subject + " " + totalMark +  " " + second_obtain + " " + (second_obtain/totalMark) * 100 + "%");
+
+// document.write(result + "<br>");
+// document.write(result2 + "<br>");
+
+// var total = 200;
+// document.write("total mark " + total + "<br>");
+
+// var obtainMark = (first_obtain + second_obtain);
+// document.write("obtain Mark " + obtainMark + "<br>");
+
+// var percentage = ((obtainMark / total) * 100);
+// document.write(percentage + "%");
